0b2ce4c0e6dd1e6f6b0763b53cdbc3ab4acca6b7
AM\MHarris1 <michael_harrison@trimble.com>
2021-05-25 16:14:40 -0400

    first push

---
+++ TestMessagerCore/IGetTimestamp.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMessagerCore
+{
+    /// <summary>
+	/// The interface used to get a formatted timestamp string from the Timestamp Service.
+	/// </summary>
+    public interface IGetTimestamp
+    {
+        string GetFormattedTimestamp();
+    }
+}
---
+++ TestMessagerCore/UtcTimestamper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestMessagerCore
+{
+    /// <summary>
+    /// A simple implementation of the Timestamp service. The timestamp returns the date & time the service started.
+    /// </summary>
+    public class UtcTimestamper : IGetTimestamp
+	{
+		DateTime startTime;
+
+		public UtcTimestamper()
+		{
+			startTime = DateTime.UtcNow;
+		}
+		
+		public string GetFormattedTimestamp()
+		{
+			TimeSpan duration = DateTime.UtcNow.Subtract(startTime);
+			return $"Service started at {startTime} ({duration:c} ago).";
+		}
+	}
+}
